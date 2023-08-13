4558e8ecdea8a79d0a9f25906d21b58a3ff0181f
Ramanand-Yadav <1112000yadav@gmail.com>
2023-08-13 22:55:00 +0530

    insertion sort

---
+++ SortingTecnique/Sorting_01/insertionSort.java
@@ -0,0 +1,20 @@
+package SortingTecnique.Sorting_01;
+
+public class insertionSort {
+    public static void main(String[] args) {
+        insertionSort(new int[]{2,13,4,1,3,6,28},7);
+    }
+    public static void insertionSort(int[] arr, int size) {
+        //Your code goes here
+        for(int i=0; i<size; i++){
+            int j=i;
+            while(j>0 && arr[j-1]>arr[j]){
+                int temp = arr[j];
+                arr[j] = arr[j-1];
+                arr[j-1] = temp;
+                j--;
+            }
+        }
+    }
+    
+}
