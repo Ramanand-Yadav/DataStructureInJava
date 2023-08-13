2fff9903da0e9f27741f0415770a29cf73f4d3fa
Ramanand-Yadav <1112000yadav@gmail.com>
2023-08-13 22:48:01 +0530

    bubble Sort

---
+++ SortingTecnique/Sorting_01/bubbleSort.java
@@ -0,0 +1,24 @@
+package SortingTecnique.Sorting_01;
+
+public class bubbleSort {
+    public static void main(String[] args) {
+        bubbleSort(new int[]{41,467,334,500,169,724,478,358,962,464},10);
+    }
+    public static void bubbleSort(int[] arr, int n) {
+        //Your code goes here
+        for(int i=n-1; i>=0; i--){
+            int didSwap = 0;
+            for(int j=0;j<i; j++){
+                if(arr[j] > arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                    didSwap = 1;
+                }
+            }
+            if(didSwap==0){
+                break;
+            }
+        }
+    }
+}
