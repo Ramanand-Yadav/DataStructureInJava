9b34c98b9d69436a1c782ac28fce597098079ad2
Ramanand-Yadav <1112000yadav@gmail.com>
2023-08-17 09:43:57 +0530

    Linear Search

---
+++ Arrays/Easy/linearSearch.java
@@ -0,0 +1,15 @@
+package Arrays.Easy;
+
+public class linearSearch {
+    public static void main(String[] args) {
+        
+    }
+    public static int linearSearch(int n, int num, int []arr){
+        // Write your code here.
+        for(int i=0; i<n; i++){
+            if(arr[i]==num) return i;
+        }
+        return -1;
+    }
+    
+}
