bdf69f01858e6e2b184ffb3e47a697ce6b27af81
Ramanand-Yadav <1112000yadav@gmail.com>
2023-08-19 20:41:36 +0530

    Moore algo

---
+++ Arrays/Medium/majorityEle.java
@@ -0,0 +1,22 @@
+public class majorityEle {
+    public static void main(String[] args) {
+        int[] arr = {3,2,3};
+
+        System.out.println(solve(arr));
+    }
+    private static int solve(int[] arr){
+        int ele=0,cnt=0;
+        for(int i=0; i<arr.length; i++){
+            if(cnt==0){
+                ele=arr[i];
+                cnt=1;
+            }else if(ele==arr[i]){
+                cnt++;
+            }else{
+                cnt--;
+            }
+        }
+        return ele;
+    }
+    
+}
