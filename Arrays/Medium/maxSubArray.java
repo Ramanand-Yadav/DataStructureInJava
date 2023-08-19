1e269106938e178e3327798b5750a24d4f226e1b
Ramanand-Yadav <1112000yadav@gmail.com>
2023-08-19 21:17:09 +0530

    KadaneAlgo

---
+++ Arrays/Medium/maxSubArray.java
@@ -0,0 +1,16 @@
+public class maxSubArray {
+    public static void main(String[] args) {
+        
+    }
+
+    private static int Kadane(int[] arr){
+        int sum=0; int maxSum=0;
+        for(int i=0; i<arr.length; i++){
+            sum+=0;
+            if(sum>maxSum) maxSum=sum;
+            if(sum<0) sum=0;
+        }
+        return maxSum;
+    }
+    
+}
