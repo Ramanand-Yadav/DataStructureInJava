a262c044f49c35a6ba4bc6332a5df2ac1b90e71e
Ramanand-Yadav <1112000yadav@gmail.com>
2023-08-24 07:35:02 +0530

    Rotate Matrix by 90 degree

---
+++ Arrays/Medium/roatateMat.java
@@ -0,0 +1,28 @@
+public class roatateMat {
+    public static void main(String[] args) {
+    
+    }
+    public static void rotateMatrix(int [][]matrix){
+        // Write your code here.
+        int n = matrix.length; 
+        // print(matrix);
+        for(int i=0; i<n; i++){
+            for(int j=i; j<n; j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        // System.out.println();
+        // print(matrix);
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n/2; j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][n-j-1];
+                matrix[i][n-j-1] = temp;
+            }
+            // reverse(matrix,0,n-1,i);
+        }
+    }
+    
+}
