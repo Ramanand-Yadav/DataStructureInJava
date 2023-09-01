e8e50695e6d11ab3dab94edac36095e7c132dac2
Ramanand-Yadav <1112000yadav@gmail.com>
2023-09-01 21:40:05 +0530

    Upper Bound

---
+++ BinarySearch/BSON1DArray/upperBound.java
@@ -0,0 +1,31 @@
+package BinarySearch.BSON1DArray;
+
+public class upperBound {
+    public static void main(String[] args) {
+        
+    }
+    public static int upperBound(int []arr, int x, int n){
+        // Write your code here.
+        int low = 0;
+        int right = n-1;
+        int ans = Integer.MAX_VALUE;
+        while(low<=right){
+            int mid = (low+right)/2;
+            if(arr[mid]>x){
+                ans = Math.min(ans, mid);
+                right = mid-1;
+            }else{
+                low = mid+1;
+            }
+        }
+        if(ans==Integer.MAX_VALUE) return arr.length;
+        return ans;
+    }
+    public static int upperBound1(int []arr, int x, int n){
+        // Write your code here.
+        for(int i=0; i<n; i++){
+            if(arr[i]>x) return i;
+        }
+        return arr.length;
+    }
+}
