4fc8864b60a95b942751891a63b0c5877364bf8a
Ramanand-Yadav <1112000yadav@gmail.com>
2023-09-02 14:27:02 +0530

    min Ele Optimal Solution addedd

--- BinarySearch/BSON1DArray/minEleRotatedSorted.java
+++ BinarySearch/BSON1DArray/minEleRotatedSorted.java
@@ -20,4 +20,29 @@ public class minEleRotatedSorted {
         }
         return ans;
     }
+    public static int findMin1(int[] nums) {
+        // return 1;
+        int ans = Integer.MAX_VALUE;
+        // for(int i=0; i<nums.length;i++){
+        //     if(ans>nums[i]){
+        //         ans = nums[i];
+        //     }
+        // }
+        // return ans;
+        int l=0,r=nums.length-1;
+        while(l<=r){
+            int mid = (l+r)/2;
+            if(nums[l]<=nums[r]){
+                ans = Math.min(nums[l], ans);
+                break;
+            }
+            if(nums[l]<=nums[mid]){
+                ans = Math.min(ans, nums[l]);
+                l = mid+1;
+            }else{
+                ans = Math.min(ans, nums[mid]);
+                r = mid-1;
+            }
+        }
+        return ans;
 }
