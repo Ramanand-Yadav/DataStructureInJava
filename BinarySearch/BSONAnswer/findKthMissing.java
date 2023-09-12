91ae9fda96501cf3544db6036a3d98af9d57cce6
Ramanand-Yadav <1112000yadav@gmail.com>
2023-09-12 22:33:40 +0530

    find Missing kth Positive Number

---
+++ BinarySearch/BSONAnswer/findKthMissing.java
@@ -0,0 +1,22 @@
+package BinarySearch.BSONAnswer;
+
+public class findKthMissing {
+    public static void main(String[] args) {
+        
+    }
+}
+
+class Solution {
+    public static int missingK(int[] vec, int n, int k) {
+        // Write your code here.
+        // nt n = arr.length; 
+        int l=0, r=n-1;
+        while(l<=r){
+            int mid = (l+r)/2;
+            int mis = vec[mid]-(mid+1);
+            if(mis<k) l = mid+1;
+            else r = mid-1;
+        }
+        return l+k;
+    }
+}
