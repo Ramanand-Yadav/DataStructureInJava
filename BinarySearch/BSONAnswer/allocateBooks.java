0efcf0396e17f2b62e6c97f08a40801cf1f8c454
Ramanand-Yadav <1112000yadav@gmail.com>
2023-09-14 19:52:22 +0530

    Allocate Books

---
+++ BinarySearch/BSONAnswer/allocateBooks.java
@@ -0,0 +1,71 @@
+package BinarySearch.BSONAnswer;
+import java.lang.reflect.Array;
+import java.util.*;
+public class allocateBooks {
+    public static void main(String[] args) {
+        ArrayList<Integer> arr = new ArrayList<>(Arrays.asList(25, 46, 28, 49, 24));
+        int n = 5;
+        int m = 4;
+        int ans = findPages(arr, n, m);
+        System.out.println("The answer is: " + ans);
+    }
+
+    public static int findPages(List<Integer> arr, int n, int m) {
+        // book allocation impossible
+        if (m > n)
+            return -1;
+
+        int low = Collections.max(arr);
+        int high = arr.stream().mapToInt(Integer::intValue).sum();
+
+        for (int pages = low; pages <= high; pages++) {
+            if (countStudents(arr, pages) == m) {
+                return pages;
+            }
+        }
+        return low;
+    }
+
+    private static int solve(List<Integer> arr, int n, int m){
+        int l = Collections.max(arr);
+        int r = arr.stream().mapToInt(Integer::intValue).sum();
+
+        if(m>n) return -1;
+        for(int i=l; i<=r; i++){
+            if(countStudents(arr,i)==m){
+                return i;
+            }
+        }
+        return l;
+    }
+
+    private static int countStudent(List<Integer>arr, int n, int pages){
+        int student = 1;
+        int pagesStudent = 0;
+        for(int i=0; i<n; i++){
+            if(pagesStudent+arr.get(i) <= pages){
+                pagesStudent+=arr.get(i);
+            }else{
+                student++;
+                pagesStudent=arr.get(i);
+            }
+        }
+        return student;
+    }
+    public static int countStudents(List<Integer> arr, int pages) {
+        int n = arr.size(); // size of array
+        int students = 1;
+        long pagesStudent = 0;
+        for (int i = 0; i < n; i++) {
+            if (pagesStudent + arr.get(i) <= pages) {
+                // add pages to current student
+                pagesStudent += arr.get(i);
+            } else {
+                // add pages to next student
+                students++;
+                pagesStudent = arr.get(i);
+            }
+        }
+        return students;
+    }
+}
