6fc0abd14718b85dcef65ec1b4d3fdc144cd43dd
Ramanand-Yadav <1112000yadav@gmail.com>
2023-09-25 21:14:55 +0530

    first node detect

---
+++ LinkedList/MediumProblemLL/firstNode.java
@@ -0,0 +1,46 @@
+package LinkedList.MediumProblemLL;
+
+public class firstNode {
+    class Node {
+        public int data;
+        public Node next;
+   
+        Node()
+        {
+            this.data = 0;
+            this.next = null;
+        }
+        Node(int data)
+        {
+            this.data = data;
+            this.next = null;
+        }
+        Node(int data, Node next)
+        {
+            this.data = data;
+            this.next = next;
+        }
+    }
+    public static void main(String[] args) {
+        
+    }
+    /* 
+    public static Node firstNode(Node head) {
+        if(head==null || head.next==null) return null;
+        if(head.next==head) return head;
+        Node slow = head.next;
+        Node fast = head.next.next;
+        while(fast!=null && fast.next!=null && slow!=fast){
+            fast = fast.next.next;
+            slow = slow.next;
+        } 
+        if(fast==null || fast.next==null) return null;
+        slow = head;
+        while(fast!=slow){
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return slow;
+    }
+    */
+}
