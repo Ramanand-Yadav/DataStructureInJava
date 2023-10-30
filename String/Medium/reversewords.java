c5d4eb9bed5855665979f84974cce8c81d6fc3c0
Ramanand-Yadav <1112000yadav@gmail.com>
2023-10-30 22:59:36 +0530

    comment out

--- String/Medium/reversewords.java
+++ String/Medium/reversewords.java
@@ -1,17 +1,17 @@
-public class reversewords {
-    public static void main(String[] args) {
-        String s = " a good   example  ";
-        String ans = reverseWords(s);
-        System.out.println("reverse word : "+ans);
-    }
-    public static String reverseWords(String s) {
-        String ans = "";
-        String[] str = s.trim().split("\\s+");
-        for(int i=str.length-1; i>0; i--){
-            ans += str[i]+" ";
-        }
-        ans += str[0];
-        return ans;
-    }
+// public class reversewords {
+//     public static void main(String[] args) {
+//         String s = " a good   example  ";
+//         String ans = reverseWords(s);
+//         System.out.println("reverse word : "+ans);
+//     }
+//     public static String reverseWords(String s) {
+//         String ans = "";
+//         String[] str = s.trim().split("\\s+");
+//         for(int i=str.length-1; i>0; i--){
+//             ans += str[i]+" ";
+//         }
+//         ans += str[0];
+//         return ans;
+//     }
     
-}
+// }
