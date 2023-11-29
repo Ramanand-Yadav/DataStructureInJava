ce7253d05aebdb19fa23cc9625102d37e9f1afbc
Ramanand-Yadav <1112000yadav@gmail.com>
2023-11-29 23:21:09 +0530

    better approach

--- StackQueue/monotonic/subarrayRange.java
+++ StackQueue/monotonic/subarrayRange.java
@@ -1,10 +1,13 @@
 package StackQueue.monotonic;
 
+import java.util.Stack;
+
 public class subarrayRange {
     public static void main(String[] args) {
         int[] arr ={1, 2, 3, 4, 5};
         int ans = rangeSum(arr);
         System.out.println(ans);
+        System.out.println(rangeSum1(arr));
     }
     public static int rangeSum(int []nums){
         // Write your code here.
@@ -22,6 +25,31 @@ public class subarrayRange {
                 }
                 ans += maxe-mine;
             }
+        }
+        return ans;
+    }
+    public static int rangeSum1(int []nums){
+        // Write your code here.
+        int n=nums.length,ans=0,j=0,k=0;
+        Stack<Integer> st=new Stack<>();
+        //for the first loop we are calculating the summation of min elements of all the subarrays
+        for(int i=0;i<=n;i++){
+         while(!st.isEmpty() && nums[st.peek()]>(i==n?Integer.MIN_VALUE: nums[i])){
+          j=st.pop();
+          k=st.isEmpty()?-1:st.peek();
+          ans-=nums[j]*(i-j)*(j-k); //here we are calculating product of the element value, the width of the subarray, and the difference in heights (number of elements greater than the popped element).
+         }
+         st.push(i);
+        }
+        st.clear();
+//here we are calculating summation of all max elements 
+        for(int i=0;i<=n;i++){
+         while(!st.isEmpty() && nums[st.peek()]<(i==n?Integer.MAX_VALUE: nums[i])){
+          j=st.pop();
+          k=st.isEmpty()?-1:st.peek();
+          ans+=nums[j]*(i-j)*(j-k);
+         }
+         st.push(i);
         }
         return ans;
     }
